4703e08b337c2986157e61a44fcf58791adc2a95
grodier <grodier3@gmail.com>
2024-01-24 20:59:11 -0500

    Attempt to update config

--- src/pages/og.png.ts
+++ src/pages/og.png.ts
@@ -1,5 +1,5 @@
 import type { APIRoute } from "astro";
-import { generateOgImageForSite } from "@utils/generateOgImages.js";
+import { generateOgImageForSite } from "@utils/generateOgImages";
 
 export const GET: APIRoute = async () =>
   new Response(await generateOgImageForSite(), {
