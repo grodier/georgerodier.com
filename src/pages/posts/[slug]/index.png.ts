ff8af36a92ebec329729d6faca961e3b66708816
grodier <grodier3@gmail.com>
2024-01-24 20:45:16 -0500

    Add file extensions to ts imports

--- src/pages/posts/[slug]/index.png.ts
+++ src/pages/posts/[slug]/index.png.ts
@@ -1,7 +1,7 @@
 import type { APIRoute } from "astro";
 import { getCollection, type CollectionEntry } from "astro:content";
-import { generateOgImageForPost } from "@utils/generateOgImages";
-import { slugifyStr } from "@utils/slugify";
+import { generateOgImageForPost } from "@utils/generateOgImages.js";
+import { slugifyStr } from "@utils/slugify.js";
 
 export async function getStaticPaths() {
   let postEntries = await getCollection("posts");
