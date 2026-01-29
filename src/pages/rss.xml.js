fe942a8063065ebeb1f1c9d19b4e2bf092d002d2
grodier <grodier3@gmail.com>
2026-01-28 21:48:14 -0500

    Fix pubDate in rss

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -2,18 +2,21 @@ import rss from "@astrojs/rss";
 import { getCollection } from "astro:content";
 import sanitizeHtml from "sanitize-html";
 import MarkdownIt from "markdown-it";
+import { sortedTimeDescending } from "@utils/postUtils";
+
 const parser = new MarkdownIt();
 
 export async function GET(context) {
   const posts = await getCollection("posts");
+  const sortedPosts = sortedTimeDescending(posts);
   return rss({
     title: "George Rodier's Blog",
     description:
       "George's thoughts on tech, building software, pragmatism, delivering value and more.",
     site: context.site,
-    items: posts.map((post) => ({
+    items: sortedPosts.map((post) => ({
       title: post.data.title,
-      pubDate: post.data.date,
+      pubDate: post.data.pubDate,
       link: `/posts/${post.id}/`,
       author: post.data.author,
       description: post.data.description,
